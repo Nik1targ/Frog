cc9ae6de2658463f8f2b4f9e2b3d80457903eaa5
Nik1targ <roan.forge@mail.ru>
2025-10-01 18:31:02 +0300

    add file

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const scrollButton = document.querySelector(scroll-top);
+const scrollButton = document.querySelector(".scroll-top");
+
+scrollButton.addEventListener(
+"click",
+() => {
+  window.scrollTo ({
+    top:0,
+    behavior: "smooth"
+  })
+  },
+  false,
+);
