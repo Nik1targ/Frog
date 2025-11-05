955eed48fc72dd8c0f0fcea9b436a8c68ec75853
Nik1targ <roan.forge@mail.ru>
2025-11-05 18:41:11 +0300

    add file

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ scrollButton.addEventListener(
   },
   false,
 );
+
+
+console.log('Привет мир');
+
+let massage;
+massage = 'Hello';
+alert(massage);
+
+let favoriteFood = "Пицца";
+
+console.log('favorite food:', favoriteFood);
+
+favoriteFood = "суши";
+
+console.log('favorite food:', favoriteFood);
+
+let age = 20;
+age = 21;
+console.log(age);
+
+const birthday = "12 мая";
+let myAge = 15;
+
+console.log('birthday:', birthday, 'myAge:', myAge);
+
+let $ = 1;
+let _ = 2;
+
+alert($ + _);
+
+let n = 123;
+n = 12.345;
+
+alert(1/0); 
+alert("не число" / 2);
