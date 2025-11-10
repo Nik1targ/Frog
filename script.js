6c4b03297531c199daf16e13adbd4bf56a19445c
Nik1targ <roan.forge@mail.ru>
2025-11-10 18:45:41 +0300

    add file

--- script.js
+++ script.js
@@ -79,3 +79,129 @@ let buttonPressed = false;
 if(buttonPressed){
   console.log("Кнопка нажата!");  
 }
+
+let tempereture = 25;
+
+if (tempereture > 20){
+  console.log("Ты выйграл!");
+}
+
+if(isRaining) {
+  console.log("Возьми зонт");
+  console.log("И надень куртку");
+}
+
+let battery = 5;
+
+if (battery < 10){
+console.log("Нужно зарядить телефон");
+} 
+else {
+  console.log("Батарея в порядке");
+}
+
+let isWeekend = false;
+
+if(isWeekend === true) {
+  console.log("Можно поспать подольше");
+}
+
+else {
+  console.log("Надо в школу");
+}
+
+
+
+let grade = 8;
+
+if(grade >= 9){
+  console.log("Отлтчно");
+}
+
+else if(grade >= 7) {
+  console.log("хорошо");
+}
+
+else if(grade >= 5) {
+  console.log("удовлетворительно");
+}
+
+else  {
+  console.log("нужно подтянуться");
+}
+
+
+
+
+
+
+
+
+let temp = 10
+
+if(temp >= 30){
+  console.log("Очень жарко");
+}
+
+else if(grade >= 20) {
+  console.log("Тепло");
+}
+
+else if(grade >= 10) {
+  console.log("Прохладно");
+}
+
+else  {
+  console.log("Холодно");
+}
+
+
+
+
+
+
+let day = 3;
+switch(day){
+  case 1:
+    console.log("Понедельник");
+    break;
+    case 2:
+      console.log("Вторник");
+    break;
+    case 3:
+      console.log("Среда");
+    break;
+    default:
+    console.log("Другой день");
+}
+
+
+let color = "red";
+switch(color){
+  case "red":
+    console.log("Крастный");
+  case "green":
+      console.log("зеленый");
+    default:
+    console.log("Другой");
+}
+
+
+
+let role = 5;
+switch (role) {
+  case 1:
+      console.log("admine");
+    break;
+    case 2:
+        console.log("user");
+
+    break;
+    case 3:
+      
+        console.log("guest");
+    break;
+    default:
+    console.log("Другой");
+}
+
