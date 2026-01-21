b79aebb1ebbbefb17f62764f5d3d4926d92af5b8
Nik1targ <roan.forge@mail.ru>
2026-01-21 19:01:29 +0300

    add file

--- Zadashi.js
+++ Zadashi.js
@@ -16,7 +16,7 @@ console.log(calcDiscount);
 
 
 
-function normalizeLogin(Login) {
+function normalizeLogins(Login) {
     const resultLogin = login.toLowerCase().trim();
 
     return resultLogin;
@@ -25,8 +25,8 @@ function normalizeLogin(Login) {
 const testlogin =" LOGINTEST";
 const login =" MYLOGIN";
 
-let normaLogin = normalizeLogin(testlogin);
-let normalizeLogin = normalizeLogin(login);
+let normaLogin = normalizeLogins(testlogin);
+let normalizeLogin = normalizeLogins(login);
 
 console.log("Normalize login", normLogin);
 console.log("Normalize second login", normalizeLoginTest);
@@ -42,5 +42,40 @@ const animals = ["cat", "parrot","dog", "elephant"];
 const min = 4;
 
 filterLongWords(animals, min);
+
+
+const users = [
+    { name: "Глеб", age: 15 },
+    { name: "Никита ", age: 16 },
+    { name: "Влад", age: 15 }
+  ];
+  
+  const formatter = (u) => `${u.name} (${u.age})`;
+  
+  const result = formatUsers(users, formatter);
+
+  //const formatUsers = (users, formatter) => {
+    //return users.map(user => formatter(user)
+ // }
+  
+  console.log(result);
+
+
+
+function isEmpty(arr) {
+    if(arr == null || ! Array.isArray(arr)) {
+        return true;
+
+    }
+    return arr.length === 0;
+}
+
+
+const isEmail = (email) => {
+    return email.includes("@")
+}
+
+
+
 
 
